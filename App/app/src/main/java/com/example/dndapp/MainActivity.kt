c3de013e34195756a4d7e5d869a9e0a55cc18076
IrisSaskia <iris.oostra@hva.nl>
2020-05-25 17:30:10 +0200

    Changed bottom tab to view icons as images and added a popup menu

--- App/app/src/main/java/com/example/dndapp/MainActivity.kt
+++ App/app/src/main/java/com/example/dndapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.dndapp
 
+import android.app.Activity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.MenuInflater
+import android.view.View
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private val mainActivity: Activity = this@MainActivity
+        ?: throw RuntimeException("getCurrentSite() can only be called when an activity is in the resumed state")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +24,29 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+        /*val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_character, R.id.navigation_bag, R.id.navigation_spells
             )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        )*/
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.itemIconTintList = null
+
+        initViews()
+    }
+
+    fun initViews() {
+        val hamburgerButton = this.findViewById(R.id.ibMenu) as ImageButton
+        hamburgerButton.setOnClickListener {
+            showPopUp(hamburgerButton)
+        }
+    }
+
+    private fun showPopUp(buttonView: View?) {
+        val popup = PopupMenu(mainActivity, buttonView)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.options_menu, popup.menu)
+        popup.show()
     }
 }
