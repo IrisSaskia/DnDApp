1ee6485d756e5585a5311b44a63bc9edcc6d04a2
IrisSaskia <iris.oostra@hva.nl>
2020-06-06 12:48:18 +0200

    The stats can be put in an array and I am now only using one (instance) of the viewmodel
    The layout is not updated, need to fix that!

--- App/app/src/main/java/com/example/dndapp/ui/character/description/CharacterDescriptionFragment.kt
+++ App/app/src/main/java/com/example/dndapp/ui/character/description/CharacterDescriptionFragment.kt
@@ -1,6 +1,8 @@
 package com.example.dndapp.ui.character.description
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,12 +19,12 @@ import kotlinx.android.synthetic.main.fragment_character_description.*
  * A simple [Fragment] subclass.
  */
 class CharacterDescriptionFragment : Fragment() {
-    private lateinit var viewModel: MainViewModel
+    //private lateinit var viewModel: MainViewModel
+    private lateinit var parentActivity: Activity
+    private lateinit var activityVarRef: MainActivity
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        viewModel = activity?.run {
-            ViewModelProvider(this).get(MainViewModel::class.java)
-        } ?: throw Exception("Invalid Activity")
+        //viewModel = activity?.run { ViewModelProvider(this).get(MainViewModel::class.java) } ?: throw Exception("Invalid Activity")
         //viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_character_description, container, false)
@@ -36,18 +38,21 @@ class CharacterDescriptionFragment : Fragment() {
     }
 
     private fun initViews() {
+        parentActivity = activity!!
+        activityVarRef = ((activity as MainActivity?)!!)
     }
 
     private fun initViewModel() {
-        viewModel.currentDnDCharacter.observe(viewLifecycleOwner, Observer { currentDnDCharacter ->
+        activityVarRef.viewModel.currentDnDCharacter.observe(viewLifecycleOwner, Observer { currentDnDCharacter ->
             if(currentDnDCharacter != null) {
                 tvBackgroundName.text = currentDnDCharacter.background
-                viewModel.getBackgroundInfo(currentDnDCharacter.background)
-                tvBackgroundInfo.text = viewModel.backgroundInfo.toString()
+                activityVarRef.viewModel.getBackgroundInfo(currentDnDCharacter.background)
+                tvBackgroundInfo.text = activityVarRef.viewModel.backgroundInfo.toString()
+                Log.d("backgroundTV", tvBackgroundInfo.text as String)
             }
         })
 
-        viewModel.backgroundInfo.observe(viewLifecycleOwner, Observer { backgroundInfo ->
+        activityVarRef.viewModel.backgroundInfo.observe(viewLifecycleOwner, Observer { backgroundInfo ->
             if(backgroundInfo != null) {
                 tvBackgroundInfo.text = backgroundInfo
             }
